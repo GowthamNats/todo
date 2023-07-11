e5df34c470de6ea80f25de6313d21a40b7fe15bf
GowthamNats <72858459+GowthamNats@users.noreply.github.com>
2023-07-11 18:08:12 +0530

    Update db.js

--- server/db.js
+++ server/db.js
@@ -4,7 +4,7 @@ require('dotenv').config()
 const pool = mysql.createPool({
     connectionLimit: 100,
     user: 'root',
-    password: 'damn',
+    password: 'root',
     host: 'localhost',
     port: 3306,
     database:'todoapp'
